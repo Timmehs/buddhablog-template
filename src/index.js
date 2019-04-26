3a3d7a653b00eb6486a89c024b6a44d907a8e5e8
Tim Sandberg <tasandberg@gmail.com>
2019-04-26 15:45:04 -0700

    Simplifyer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-import React from 'react'
-import { render, hydrate } from 'react-dom'
-import './style/main.scss'
 import App from './components/App'
 
-const rootElement = document.getElementById('buddhablog')
-
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement)
-} else {
-  render(<App />, rootElement)
-}
+export default App
